8dd77eae004065258e4cf7a2f400de250c04fb13
Vivian-Santana <santanavivian245@gmail.com>
2021-12-05 23:34:20 -0300

    continuação do blog pessoal

--- src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,29 +6,32 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "postagem")
 
 public class Postagem {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-  
+
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String titulo;
-	
+
 	@NotNull
 	@Size(min = 5, max = 500)
 	private String texto;
-	
+
 	public long getId() {
 		return id;
 	}
@@ -58,10 +61,22 @@ public class Postagem {
 		return date;
 	}
 
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
 	public void setDate(Date date) {
 		this.date = date;
 	}
 
-	private Date date = new java.sql.Date (System.currentTimeMillis());
-	
+	private Date date = new java.sql.Date(System.currentTimeMillis());
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
 }
